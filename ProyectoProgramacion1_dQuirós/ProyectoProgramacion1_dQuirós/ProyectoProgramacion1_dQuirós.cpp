c4a6ca2c8cd9097bec158ddf8b8cb50648d63b64
Danna-QR <danna.quiros.ramirez@est.una.ac.cr>
2025-09-16 03:02:50 -0600

    creación del objeto en el main

---
+++ ProyectoProgramacion1_dQuirós/ProyectoProgramacion1_dQuirós/ProyectoProgramacion1_dQuirós.cpp
@@ -0,0 +1,13 @@
+#include "game.h"
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    
+        game obj = game();
+        obj.showPrincipalWindow();
+}
