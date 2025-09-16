a20af0de9a71ea1607e8fa97fa5adf39bd6aac5e
Danna-QR <danna.quiros.ramirez@est.una.ac.cr>
2025-09-16 00:33:49 -0600

    Implementación de los métodos de la clase gem, creación de texturas y sprites de las gemas

--- ProyectoProgramacion1_dQuirós/ProyectoProgramacion1_dQuirós/board.cpp
+++ ProyectoProgramacion1_dQuirós/ProyectoProgramacion1_dQuirós/board.cpp
@@ -1,1 +1,42 @@
 #include "board.h"
+#include "gem.h"
+#include <SFML/Graphics.hpp>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+using namespace std;
+
+void board::fillBoard()
+{
+    srand(time(0));
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            boardMatrix[i][j] = rand() % gemType;
+        }
+    }
+}
+
+void board::createGemTextures()
+{
+    gemTextures[0].setType(0, "assets/gemaRoja.png");
+    gemTextures[1].setType(1, "assets/gemaVerde.png");
+    gemTextures[2].setType(2, "assets/gemaAzul.png");
+    gemTextures[3].setType(3, "assets/gemaAmarilla.png");
+    gemTextures[4].setType(4, "assets/gemaMorada.png");
+}
+
+void board::createGemSprites()
+{
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+
+            sf::Texture& spriteTexture = gemTextures[boardMatrix[i][j]].getTexture();
+
+            sprites[i][j].setTexture(spriteTexture);
+            sprites[i][j].setPosition(j * pixelSize, i * pixelSize);
+            sprites[i][j].setScale(
+                pixelSize / (float)(spriteTexture).getSize().x,
+                pixelSize / (float)(spriteTexture).getSize().y);
+        }
+    }
+}
--- ProyectoProgramacion1_dQuirós/ProyectoProgramacion1_dQuirós/gem.cpp
+++ ProyectoProgramacion1_dQuirós/ProyectoProgramacion1_dQuirós/gem.cpp
@@ -1,1 +1,33 @@
 #include "gem.h"
+using namespace std;
+
+gem::gem()
+{
+    gemType = 0;
+}
+
+
+void gem::setType(int gemIndex, const string& textureFile)
+{
+    gemType = gemIndex;
+
+    if (!texture.loadFromFile(textureFile)) {
+        cout << "Error cargando textura: " << textureFile << "\n";
+    }
+    sprite.setTexture(texture);
+}
+
+int gem::getType() const
+{
+    return gemType;
+}
+
+sf::Sprite& gem::getSprite()
+{
+    return sprite;
+}
+
+sf::Texture& gem::getTexture()
+{
+    return texture;
+}
