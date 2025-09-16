6e14a10c40cd802988b9d98b6947750f7460a137
Danna-QR <danna.quiros.ramirez@est.una.ac.cr>
2025-09-15 23:44:14 -0600

    declaración de algunos atributos y metodos de las clases

--- ProyectoProgramacion1_dQuirós/ProyectoProgramacion1_dQuirós/board.h
+++ ProyectoProgramacion1_dQuirós/ProyectoProgramacion1_dQuirós/board.h
@@ -1,5 +1,27 @@
 #pragma once
+#include "gem.h"
+const int rows = 8;
+const int cols = 8;
+const int pixelSize = 70;
+const int gemType = 5;
 class board
 {
+private:
+    int boardMatrix[rows][cols];
+    gem gemTextures[gemType];     // ahora es un array estático
+    sf::Sprite sprites[rows][cols];
+
+public:
+    void fillBoard();
+    void createGemTextures();
+    void createGemSprites();
+    bool verifyMatch();
+    void draw(sf::RenderWindow& window);
+
+
+    // ?? Nuevas funciones
+    bool isAdjacent(int rowFirstClick, int colFirstClick, int rowSecondClick, int colSecondClick);
+    void swapGems(int rowFirstClick, int colFirstClick, int rowSecondClick, int colSecondClick);
+
 };
 
--- ProyectoProgramacion1_dQuirós/ProyectoProgramacion1_dQuirós/game.h
+++ ProyectoProgramacion1_dQuirós/ProyectoProgramacion1_dQuirós/game.h
@@ -1,5 +1,27 @@
-#pragma once
+﻿#pragma once
+#include "board.h"
+#include <SFML/Graphics.hpp>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+
 class game
 {
-};
+private:
+	board board;
+	// 🔹 Variables de click
+	bool firstClick = true;
+	bool mousePressed = false;
+	int rowFirstClick, colFirstClick;
+	int rowSecondClick;
+	int colSecondClick;
+	int movements = 20;
 
+public:
+	game();
+	void showPrincipalWindow();
+	void showBoardWindow();
+	void  decreaseMovements(int& movements);
+	void handleMouseClick(sf::RenderWindow& window, int& movements);
+};
